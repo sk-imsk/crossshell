c5759309e51aef087c4ca97ace54acb310da8eb0
sk-imsk <pikachududee@gmail.com>
2025-06-04 18:23:14 +1000

    Initial Commit

---
+++ crossshell/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  crossshell
+//
+//  Created by evan matthews on 4/6/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
