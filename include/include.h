3dab031aa90ee09d715affce8ce75cefd5a862e9
sk-imsk <pikachududee@gmail.com>
2025-06-06 09:00:00 +1000

    .

---
+++ include/include.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include "shell.h"
+#include "builtin.h"
+#include "process.h"
+#include <unistd.h>
